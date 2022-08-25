6a4d0fd015e6fa66aabb9135be31ed1d8a5ac684
zig2022 <zassefa663@gmail.com>
2022-08-25 20:09:38 +0300

    Page redirection on home Page is finished

--- client/src/components/Common/Home.js
+++ client/src/components/Common/Home.js
@@ -18,7 +18,7 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                Happy Coding! {};
+                Fast job Alart! {};
            </div>
         )
     }
